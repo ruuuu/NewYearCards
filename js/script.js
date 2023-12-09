b9195f88c900dbf32e78fc765937856d9d018d0b
ruuuu <rufinka_91@mail.ru>
2023-12-09 14:36:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 const swiperThumb = new Swiper('.gift__swiper--thumb', {  // нижиний слайдер
-  spaceBetween: 16,  //расстяние между слайдами
-  slidesPerView: 6,  // число слайдев котые отображаются на  странице
+  spaceBetween: 12,  // расстяние между слайдами
+  slidesPerView: 5,  // число слайдев котые отображаются на  странице
   freeMode: true,
   watchSlidesProgress: true,  // будет ил добавляться класс 'swiper-slide-thumb-active' у слайда если он сейчас активный
+  breakpoints: {  // адаптив:
+      320: {
+         slidesPerView: 5,
+         spaceBetween: 12,
+      },
+      1141: {
+         slidesPerView: 6,
+         spaceBetween: 16,
+      },
+
+  }
 });
 
 
